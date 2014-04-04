ca889614a7eb2e9b61691732957bf03e22c7212a
MrHowl <mrhowl@foxmail.com>
2014-04-04 09:50:32 +0800

    add function delete

--- heatmapMobile/index.php
+++ heatmapMobile/index.php
@@ -1,41 +1,28 @@
 <!DOCTYPE HTML>
-
 <html>
-
 <head>
-
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-
-<title>HeatmapMobile</title>
-
-<link rel="stylesheet" href="css/style.css" type="text/css">
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js "></script>
-<script type="text/javascript" src="js/heatmap.js"></script>
-
-
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>HeatmapMobile</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+	<script type="text/javascript" src="js/heatmap.js"></script>
 </head>
 
 <body>
-
-<p>注意图片太小的话，看不到进度条</p>
-
-<input type="file" id="filesInput" accept="image/*" capture/>
-<input type="file" id="filesInputMultiple" accept="image/*"  multiple capture/>
-
-<br><br>
-
-<a href="javascript:;" id="btnUpload">开始上传</a>
-
-<p id="info"></p>
-
-<label>读取进度：</label><progress id="Progress" value="0" max="100"></progress>
-
-<span id="percent"></span>
-
-<p id="uploadSpeed"></p>
-
-<ul id="imageBox"></ul>
-
+	<div id="wrapper">
+		<p>注意图片太小的话，看不到进度条</p>
+		<input type="file" id="filesInput" accept="image/*" capture/>
+		<input type="file" id="filesInputMultiple" accept="image/*"  multiple capture/>
+		<a href="javascript:void(0);" id="deleteFiles" class="deleteFiles">删除</a>
+		<br><br>
+		<a href="javascript:void(0);" id="btnUpload">开始上传</a>
+		<p id="info"></p>
+		<label>读取进度：</label><progress id="Progress" value="0" max="100"></progress>
+		<span id="percent"></span>
+		<p id="uploadSpeed"></p>
+		<ul id="imageBox"></ul>
+		<a href="javascipt:void(0);" id = "test">test</a>
+		<p id = "testp"></p>
+	</div>
 </body>
-
 </html>
--- heatmapMobile/js/heatmap.js
+++ heatmapMobile/js/heatmap.js
@@ -1,26 +1,40 @@
 $(document).ready(function() {
     
-    function $(id) {
+    var j = jQuery.noConflict(true);
+
+    j("#test").click(function(event) {
+        /* Act on the event */
+        alert("a");
+    });
+
+
+    function $(id) {//获取元素的方法
 
         return document.getElementById(id);
 
     }
 
-    var filesInput = $("filesInput"),
+    var filesInput = $("filesInput"),//单选按钮
 
-        filesInputMultiple = $("filesInputMultiple"),
+        filesInputMultiple = $("filesInputMultiple"),//多选按钮
 
-        info = $("info"),
+        info = $("info"),//不支持Html5；上传失败；上传成功
 
-        imageBox = $("imageBox"),
+        imageBox = $("imageBox"),//不是图片;图片的信息
 
-        btnUpload = $("btnUpload"),
+        btnUpload = $("btnUpload"),//上传按钮
 
-        progress = $("Progress"),
+        progress = $("Progress"),//上传进度
 
-        percent = $("percent"),
+        percent = $("percent"),//上传百分比
 
-        uploadSpeed = $("uploadSpeed");
+        uploadSpeed = $("uploadSpeed"),//上传速度
+
+        wrapper = $("wrapper"),//总的父元素
+
+        testp = $("testp"),
+
+        deletefiles = $("deleteFiles");//删除图片
 
     //定义存放图片对象的数组,调用了$函数
 
@@ -30,14 +44,14 @@ $(document).ready(function() {
 
     var isUpload = false;
 
-    if (window.File && window.FileList && window.FileReader && window.Blob) {
+    if (window.File && window.FileList && window.FileReader && window.Blob) {//浏览器支持Html5
         
-        filesInput.addEventListener("change", getFiles, false);
+        filesInput.addEventListener("change", getFiles, false);//监听单选按钮
 
-        filesInputMultiple.addEventListener("change", getFiles, false);
+        filesInputMultiple.addEventListener("change", getFiles, false);//监听多选按钮
     } 
 
-    else {
+    else {//不支持html5的提示
 
         info.innerHTML = "您的浏览器不支持HTML5长传";
 
@@ -45,19 +59,47 @@ $(document).ready(function() {
 
     }
     
-    btnUpload.addEventListener("click", uploadFun, false);
+    btnUpload.addEventListener("click", uploadFun, false);//监听上传按钮
+    
+    imageBox.addEventListener("click", deleteFiles, false);//监听删除按钮
 
     //定义获取图片信息的函数
 
-    function getFiles(e) {
-
-        isUpload = false;
-
+    function deleteFiles(e) {
+        
         e = e || window.event;
+
+        tar = e.target;
+
+        className = tar.className;
+
+        if (className == "deleteFiles") {
+
+            var index = j(tar).parent().index();
+
+            j(tar).parent().remove();
+
+            testp.innerHTML = index;
+
+            uploadImgArr.splice(index,1);
+
+            //console.log(uploadImgArr);
+
+        }
+
+        return;
+
+    }
+
+    function getFiles(e) {//选择图片后做的处理
+
+        isUpload = false;//图片标记为未上传
+
+        e = e || window.event;//选择好的图片，是一个对象
 
         //获取file input中的图片信息列表
 
-        var files = e.target.files;
+        var files = e.target.files;//对象中的一组数据：文件
 
             //验证是否是图片文件的正则
 
@@ -68,30 +110,30 @@ $(document).ready(function() {
         for (var i = 0, f; f = files[i]; i++) {//files[i]存在即可循环，针对多选图片的写法
 
             //把这个if判断去掉后，也能上传别的文件
-            if (!reg.test(f.type)) {
+            /*if (!reg.test(f.type)) {//判断上传的是否是图片，input中已经用到了accept属性，故此处不需要
 
                 imageBox.innerHTML += "<li>你选择的" + f.name + "文件不是图片</li>";
                 //跳出循环
                 continue;
-            }
+            }*/
 
             //console.log(f);
 
-            uploadImgArr.push(f);
+            uploadImgArr.push(f);//将图片文件加至存储图片的数组，注意uploadImgArr是一个外部变量
 
-            var reader = new FileReader();//为当前的文件建立一个FileReader
+            var reader = new FileReader();//为当前的文件建立一个FileReader,用来获取文件的信息
 
             //类似于原生JS实现tab一样（闭包的方法）
 
-            reader.onload = (function(file) {
+            reader.onload = (function(file) {//处理第i个被选择的文件
 
                 //获取图片相关的信息
 
-                var fileSize = (file.size / 1024).toFixed(2) + "K",
+                var fileSize = (file.size / 1024).toFixed(1) + "K",//图片的大小，保留到小数点后一位
 
-                    fileName = file.name,
+                    fileName = file.name;//图片的名字，保留五位
 
-                    fileType = file.type;
+                    //fileType = file.type;//图片的类型，不需要
 
                 //console.log(fileName)
 
@@ -101,21 +143,21 @@ $(document).ready(function() {
 
                     //获取图片的宽高
 
-                    var img = new Image();
+                    var img = new Image();//预览图片
 
-                    img.addEventListener("load", imgLoaded, false);
+                    img.addEventListener("load", imgLoaded, false);//文件的监听
 
-                    img.src = e.target.result;
+                    img.src = e.target.result;//第i个文件的地址
 
                     function imgLoaded() {
 
-                        imgWidth = img.width;
+                        //imgWidth = img.width;//文件的宽度，不需要
 
-                        imgHeight = img.height;
+                        //imgHeight = img.height;//文件的高度，不需要
 
                         //图片加载完成后才能获取imgWidth和imgHeight
 
-                        imageBox.innerHTML += "<li><img src='" + e.target.result + "' alt='" + fileName + "' title='" + fileName + "'> 图片名称是：" + fileName + ";图片的的大小是：" + fileSize + ";图片的类型是：" + fileType + ";图片的尺寸是：" + imgWidth + " X " + imgHeight + "</li>";
+                        imageBox.innerHTML += "<li><img src='" + e.target.result + "' alt='" + fileName + "' title='" + fileName + "'> 图片名称是：" + fileName + ";图片的的大小是：" + fileSize + "<br/><a href='javascript:void(0);' class='deleteFiles'>delete</a></li>";
 
                     }
 
@@ -126,6 +168,8 @@ $(document).ready(function() {
             //读取文件内容
 
             reader.readAsDataURL(f);
+
+            //console.log(uploadImgArr);
 
         }
 
@@ -241,7 +285,6 @@ $(document).ready(function() {
         }
         fun();
     }
-
 });
 
 //定义获取对象的方法
